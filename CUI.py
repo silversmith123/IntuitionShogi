6209695f414bce86296458b8836120b7e064813a
mac <t.bridgestone.m@gmail.com>
2021-05-15 13:19:55 +0900

    特徴量の差分更新

--- CUI.py
+++ CUI.py
@@ -15,23 +15,23 @@ class CUI:
 		self.model.compile(optimizer="Adam", loss="mse", metrics=["mae"], run_eagerly=True)
 
 	def eval(self):
-		features = preprocessing.board_to_features(self.board)
-		inputs_koma_b = np.array([features['koma_b']])
-		inputs_koma_w = np.array([features['koma_w']])
-		inputs_hand_koma_b = np.array([features['hand_koma_b']])
-		inputs_hand_koma_w = np.array([features['hand_koma_w']])
-		inputs_atk_koma_b = np.array([features['atk_koma_b']])
-		inputs_atk_koma_w = np.array([features['atk_koma_w']])
-		inputs_difence_koma_b = np.array([features['difence_koma_b']])
-		inputs_difence_koma_w = np.array([features['difence_koma_w']])
+		inputs_koma_b = np.array([self.features['koma_b']])
+		inputs_koma_w = np.array([self.features['koma_w']])
+		inputs_hand_koma_b = np.array([self.features['hand_koma_b']])
+		inputs_hand_koma_w = np.array([self.features['hand_koma_w']])
+		inputs_atk_koma_b = np.array([self.features['atk_koma_b']])
+		inputs_atk_koma_w = np.array([self.features['atk_koma_w']])
+		inputs_difence_koma_b = np.array([self.features['difence_koma_b']])
+		inputs_difence_koma_w = np.array([self.features['difence_koma_w']])
 		eval = float(self.model.call({'inputs_koma_b': inputs_koma_b, 'inputs_koma_w': inputs_koma_w, 'inputs_hand_koma_b':inputs_hand_koma_b, 'inputs_hand_koma_w':inputs_hand_koma_w, 'inputs_atk_koma_b': inputs_atk_koma_b, 'inputs_atk_koma_w': inputs_atk_koma_w, 'inputs_difence_koma_b':inputs_difence_koma_b, 'inputs_difence_koma_w':inputs_difence_koma_w}))
 		return eval
 
 	def forward(self, move):
-		self.board.push(move)
+		self.features = preprocessing.update_features(self.board, move, self.features)
 
 	def back(self):
 		self.board.pop()
+		self.features = preprocessing.board_to_features(self.board)
 
 	def input(self, action):
 		if action == "back":
@@ -41,7 +41,6 @@ class CUI:
 			self.forward(move)
 
 	def show_feature(self):
-		features = preprocessing.board_to_features(self.board)
 		def _print(feature):
 			for rank in range(9):
 				line = ''
@@ -55,12 +54,12 @@ class CUI:
 
 		for type in ['koma_b', 'koma_w', 'atk_koma_b', 'atk_koma_w', 'difence_koma_b', 'difence_koma_w']:
 			print(type)
-			_print(features[type])
+			_print(self.features[type])
 
 		for type in ['hand_koma_b', 'hand_koma_w']:
 			print(type)
 			line = ''
-			for koma in features[type]:
+			for koma in self.features[type]:
 				line += str(int(koma))
 			print(line)
 
@@ -83,6 +82,7 @@ class CUI:
 		print('駒の表現 玉:K 飛車:R 角:B 金:G 銀:S 桂馬:N 香車:L 歩:P')
 		sfen = input('初期局面をsfenで入力してください。\n')
 		self.board = shogi.Board(sfen)
+		self.features = preprocessing.board_to_features(self.board)
 		self.show()
 		while True:
 			action = input('1手進める場合は指し手、1手戻す場合はback、終了する場合はexitを入力してください。\n')
