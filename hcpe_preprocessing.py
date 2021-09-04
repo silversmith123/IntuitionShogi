5f32e379c80c53fbd61f03ecdb2be563a3c41ae8
mac <t.bridgestone.m@gmail.com>
2021-09-04 21:38:46 +0900

    手番対応

--- hcpe_preprocessing.py
+++ hcpe_preprocessing.py
@@ -3,12 +3,20 @@ import cshogi
 
 def cnn_board_to_features(board):
 
-	# 駒の配列
+	# 駒の配列 手番の側を先手とする
 	koma = np.array(board.pieces, dtype=int)
 	koma_board = np.identity(31, dtype = float)[koma]
+	if board.turn == cshogi.WHITE:
+		koma_board = koma_board[::, [0,17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 ,28, 29, 30, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+		koma_board = koma_board[::-1, ::]
 	koma_board = koma_board.reshape(9, 9, 31)
-	hand_koma_b = board.pieces_in_hand[cshogi.BLACK]
-	hand_koma_w = board.pieces_in_hand[cshogi.WHITE]
+
+	if board.turn == cshogi.BLACK:
+		hand_koma_b = board.pieces_in_hand[cshogi.BLACK]
+		hand_koma_w = board.pieces_in_hand[cshogi.WHITE]
+	else:
+		hand_koma_b = board.pieces_in_hand[cshogi.WHITE]
+		hand_koma_w = board.pieces_in_hand[cshogi.BLACK]
 
 	features = {}
 	features['koma_board'] = koma_board
